edd0769495aa6338a27bc84edec6f5b9a0aa126e
geleeroyale <krassvs@gmail.com>
2020-02-26 22:45:01 +0400

    add commons stack styling

--- src/components/faq.js
+++ src/components/faq.js
@@ -3,15 +3,14 @@ import { documentToReactComponents } from "@contentful/rich-text-react-renderer"
 import styled from "styled-components"
 import Collapsible from "react-collapsible"
 
+import ShareButton from "./ShareButton"
+
 const ContentContainer = styled.div`
-  padding: 20vh 0;
+  padding: 3rem 0;
   max-width: 960px;
   margin: 0 auto;
   justify-content: center;
-
-  a {
-    color: powderblue;
-  }
+  color: #67de69;
 `
 
 const ContentItem = styled.div`
@@ -19,13 +18,14 @@ const ContentItem = styled.div`
   grid-template-columns: 1fr;
   grid-template-rows: auto auto;
   align-self: center;
-  border-radius: 10px;
   background-position: center;
   background-attachment: fixed;
-  color: black;
+  background-color: #17171766;
+  color: white;
   transition: 0.8s cubic-bezier(0.2, 0.8, 0.2, 1);
-  padding: 0.5rem;
-  border: 2px solid #00000000;
+  margin: 0.5rem;
+  border: 5px solid #17171766;
+  border-radius: 10px 10px 0px 0px;
   @media (max-width: 990px) {
     justify-items: center;
     justify-self: center;
@@ -33,10 +33,20 @@ const ContentItem = styled.div`
     grid-gap: 0;
   }
 
+  h2 {
+    margin: 0;
+    padding: 0.5rem;
+    color: #67de69;
+  }
   .Collapsible__trigger {
     :hover {
       cursor: pointer;
     }
+  }
+
+  .Collapsible__contentInner {
+    background: #17171766;
+    color: white;
   }
 `
 
@@ -44,24 +54,51 @@ const LongDescription = styled.div`
   justify-self: center;
   margin: 0;
   padding: 2rem;
-  p {
-    margin: 0;
-  }
   @media (max-width: 990px) {
     max-width: 1fr;
   }
 `
 
+const Category = styled.h2`
+  color: white;
+`
+
 const Faq = ({ data }) => {
+  const hash = window.location.hash
+  console.log(hash)
   return (
     <ContentContainer>
+      <Category>{data[0].node.category.category}</Category>
       {data.map(edges => (
         <ContentItem id={edges.node.linkId} key={edges.node.id}>
-          <Collapsible trigger={edges.node.question}>
-            <LongDescription>
-              {documentToReactComponents(edges.node.answer.json)}
-            </LongDescription>
-          </Collapsible>
+          {hash === `#${edges.node.linkId}` ? (
+            <Collapsible
+              trigger={
+                <span>
+                  <h4>{edges.node.question}</h4>
+                </span>
+              }
+              open={true}
+            >
+              <LongDescription>
+                {documentToReactComponents(edges.node.answer.json)}
+              </LongDescription>
+              <ShareButton data={edges.node.linkId} />
+            </Collapsible>
+          ) : (
+            <Collapsible
+              trigger={
+                <span>
+                  <h2>{edges.node.question}</h2>
+                </span>
+              }
+            >
+              <LongDescription>
+                {documentToReactComponents(edges.node.answer.json)}
+              </LongDescription>
+              <ShareButton data={edges.node.linkId} />
+            </Collapsible>
+          )}
         </ContentItem>
       ))}
     </ContentContainer>
--- src/components/header.js
+++ src/components/header.js
@@ -1,35 +1,74 @@
-import { Link } from "gatsby"
+import { Link, useStaticQuery, graphql } from "gatsby"
 import PropTypes from "prop-types"
 import React from "react"
+import styled from "styled-components"
 
-const Header = ({ siteTitle }) => (
-  <header
-    style={{
-      background: `rebeccapurple`,
-      marginBottom: `1.45rem`,
-    }}
-  >
-    <div
+import SearchBar from "../components/SearchBar"
+
+import logo from "../images/commonsstack-typelogo.svg"
+
+const HeaderContainer = styled.div`
+  display: grid;
+  grid-template-columns: 1fr 3fr 1fr;
+`
+
+const SearchContainer = styled.div`
+  max-width: 960px;
+  margin: 0 auto;
+  padding: 0.5rem 0;
+`
+
+const Logo = styled.img``
+const Title = styled.p`
+  justify-self: center;
+  align-self: center;
+`
+
+const Header = ({ siteTitle }) => {
+  const data = useStaticQuery(graphql`
+    query SiteSearchQuery {
+      search: localSearchFaq {
+        index
+        store
+      }
+    }
+  `)
+  const jsonObject = JSON.parse(data.search.store)
+  return (
+    <header
       style={{
-        margin: `0 auto`,
-        maxWidth: 960,
-        padding: `1.45rem 1.0875rem`,
+        background: `#17171766`,
+        marginBottom: `1.45rem`,
       }}
     >
-      <h1 style={{ margin: 0 }}>
-        <Link
-          to="/"
-          style={{
-            color: `white`,
-            textDecoration: `none`,
-          }}
-        >
-          {siteTitle}
-        </Link>
-      </h1>
-    </div>
-  </header>
-)
+      <HeaderContainer
+        style={{
+          margin: `0 auto`,
+          maxWidth: 960,
+          padding: `1.45rem 1.0875rem`,
+        }}
+      >
+        <a href="https://commonsstack.org">
+          <Logo src={logo} />
+        </a>
+        <Title style={{ margin: "0, auto", textAlign: `center` }}>
+          <Link
+            to="/"
+            style={{
+              color: `#A6A6A6`,
+              textDecoration: `none`,
+            }}
+          >
+            {siteTitle}
+          </Link>
+        </Title>
+      </HeaderContainer>
+      <SearchContainer>
+        <SearchBar index={data.search.index} store={jsonObject} />
+      </SearchContainer>
+    </header>
+  )
+}
 
 Header.propTypes = {
   siteTitle: PropTypes.string,
